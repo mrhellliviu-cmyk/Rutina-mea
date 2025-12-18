787668d2dbb6d05ead0fadcabc5563374a6a6a09
mrhellliviu-cmyk <mr_hell_liviu@yahoo.com>
2025-12-18 15:34:55 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,18 @@
-const CACHE = "rutina-mea-v2";
+const CACHE = "rutina-mea-v3";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./sw.js"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
+});const CACHE = "rutina-mea-v2";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./sw.js"];
 
 self.addEventListener("install", (e) => {
